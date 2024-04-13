2ec598061cf6b3bc83407f0c36dea287af23bd8c
Cat Nguyen <112023885+NguyenCatNguyen@users.noreply.github.com>
2024-04-12 21:33:36 -0500

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ require_once ("includes/login_view.php");
   if (!isset($_SESSION["user_id"])) { ?>
     <div class="Login-wrapper">
       <form action="includes/login.php" method="post">
-        <h1 class="title">Log1n</h1>
+        <h1 class="title">LogIn</h1>
         <div class="input-box">
           <input type="text" name="username" placeholder="Username" required>
         </div>
@@ -37,27 +37,26 @@ require_once ("includes/login_view.php");
       </form>
     </div>
   <?php } ?>
-
   <?php
   check_login_errors();
   ?>
-  <h3>Signup</h3>
-  <form action="includes/signup.php" method="post">
-    <input type="text" name="username" placeholder="Username"> <br>
-    <input type="password" name="pwd" placeholder="Password"> <br>
-    <!-- prolly don't need an email but we could add it later -->
-    <!-- <input type="text" name="email" placeholder="E-mail"> -->
-    <button>Signup</button>
-  </form>
   <?php
   check_signup_errors();
   ?>
   <?php
   if (isset($_SESSION["user_id"])) { ?>
-    <h3>Logout</h3>
-    <form action="includes/logout.php" method="post">
-      <button>Logout</button>
-    </form>
+    <div class="Login-wrapper">
+        <form action="includes/logout.php" method="post">
+          <h1 class="title">SignUp</h1>
+          <div class="input-box">
+            <input type="text" name="username" placeholder="Username" required>
+          </div>
+          <div class="input-box">
+            <input type="password" name="pwd" placeholder="Password" required>
+          </div>
+          <button type="submit" class="btn">Sign Up</button>
+        </form>
+      </div>
   <?php } ?>
 </body>
 
