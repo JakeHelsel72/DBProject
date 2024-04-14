44a159cb5f13d05d9132742aedb37a252e831457
Cat Nguyen <112023885+NguyenCatNguyen@users.noreply.github.com>
2024-04-13 21:43:13 -0500

    Update postviewer.php

--- postviewer.php
+++ postviewer.php
@@ -13,21 +13,31 @@ $teststr = "test";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo $teststr ?></title>
-    <link rel="stylesheet" href="post.css">
+    <link rel="stylesheet" href="postviewer.css">
   </head>
   <body>
+    <nav class="row">
+        <img src="./WebDisplay/Assests/Logo.png" class="logo_img" alt="">
+        <ul class="nav_links">
+            <li class="link link__hover-effect">Experience</li>
+            <li class="link link__hover-effect">Favorite</li>
+            <li class="link btn">Sign In</li>
+        </ul>
+    </nav>
   <?php
         output_username();
     ?>
   <?php
   if (isset($postId)) { ?>
-    <p> <?php echo $postId ?><?php ?> </p>
-    <img class="post-img" src="./upload/<?php echo $postId .".". findFileExtensionByPostID($pdo, $postId); ?>" alt="FILL IN">
-    <p> <?php echo findTitleByPostID($pdo, $postId); ?><?php ?> </p>
-    <p> <?php echo findUsernameByPostID($pdo, $postId); ?><?php ?> </p>
-    <p> <?php echo findDescriptionByPostID($pdo, $postId); ?><?php ?> </p>
-    <p> <?php echo findLinkByPostID($pdo, $postId); ?><?php ?> </p>
-
+    <div class="mainpost-wrapper">
+        <img src="./upload/<?php echo $postId .".". findFileExtensionByPostID($pdo, $postId); ?>" alt="FILL IN" class="post-img">
+        <div class="post-info">
+            <p class="post-title"> <?php echo findTitleByPostID($pdo, $postId); ?><?php ?></p>
+            <p class="postid">ID number: <?php echo $postId ?><?php ?></p>
+            <p class="username">Post by: <?php echo findUsernameByPostID($pdo, $postId); ?><?php ?></p>
+            <p class="post-description"><?php echo findDescriptionByPostID($pdo, $postId); ?><?php ?></p>
+            <p class="post-link"><?php echo findLinkByPostID($pdo, $postId); ?><?php ?></p>
+        </div>
     </div>
     <?php } else { ?>
         <p> postId unset </p>
