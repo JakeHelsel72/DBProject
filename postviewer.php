40ad2f9c0b38a4f1e89f09ccacf6ebdb97e093d9
NguyenCatNguyen <catngn11@gmail.com>
2024-04-16 01:49:34 -0500

    Update postviewer.php

--- postviewer.php
+++ postviewer.php
@@ -6,7 +6,6 @@ require_once("includes/postviewerutil.php");
 $postId = $_GET['postId'];
 $title = findTitleByPostID($pdo, $postId);
 $username = findUsernameByPostID($pdo, $postId); // this is posting user
-$UID = findUIDByPostID($pdo, $postId);
 $description = findDescriptionByPostID($pdo, $postId);
 $link = findLinkByPostID($pdo, $postId);
 $link = fix_link($link); // adds http:// or https://
@@ -18,8 +17,11 @@ $indexurl = "http://$_SERVER[HTTP_HOST]/{$baseDir}/index.php";
 
 <!DOCTYPE html>
 <!-- <script>
-    function toggleLike(postId) {
-    var likeIcon = document.getElementById('likeIcon');
+    
+</script> -->
+<script>
+  function toggleLike() {
+    var likeIcon = document.getElementById('likeButton');
 
     // Toggle between 'full-heart' and 'empty-heart' classes
     if (likeIcon.classList.contains('fa-regular')) {
@@ -29,8 +31,8 @@ $indexurl = "http://$_SERVER[HTTP_HOST]/{$baseDir}/index.php";
         likeIcon.classList.remove('fa-solid');
         likeIcon.classList.add('fa-regular');
     }
-</script> -->
-<script>
+  }
+
 function submitLikeForm() {
     var formData = new FormData(document.getElementById('likeForm'));
     var xhr = new XMLHttpRequest();
@@ -73,9 +75,7 @@ function submitLikeForm() {
             <li class="link link__hover-effect">Experience</li>
             <li class="link link__hover-effect">Favorite</li>
 
-            <a href="index.php" class="link btn">
-                <li>Sign In</li>
-            </a>
+            <a href="index.php" class="link btn">Sign In</a>
         </ul>
     </nav>
   <?php
@@ -86,10 +86,10 @@ function submitLikeForm() {
     <div class="mainpost-wrapper">
         <img src="./upload/<?php echo $postId .".". findFileExtensionByPostID($pdo, $postId); ?>" alt="FILL IN" class="post-img">
         <div class="post-info">
-            <p class="post-title"> <?php echo $title ?><?php ?></p>
+            <p class="post-title"> <?php echo findTitleByPostID($pdo, $postId); ?><?php ?></p>
             <!-- <p class="postid">ID number: <?php echo $postId ?><?php ?></p> --> 
-            <a class="username" href="accountviewer.php?userId=<?php echo $UID ?><?php ?>">Post by: <?php echo $username ?><?php ?></a>
-            <p class="post-description"><?php echo $description ?><?php ?></p>
+            <p class="username">Post by: <?php echo findUsernameByPostID($pdo, $postId); ?><?php ?></p>
+            <p class="post-description"><?php echo findDescriptionByPostID($pdo, $postId); ?><?php ?></p>
             <p class="post-link">
                 <a class="anchor-link" href="<?php echo $finalLink?>" target="_blank"><?php echo findLinkByPostID($pdo, $postId); ?></a>
             </p>
@@ -99,7 +99,7 @@ function submitLikeForm() {
             <form id="likeForm" action="like.php" method="POST">
                 <input type="hidden" name="userId" value="<?php echo isset($_SESSION['user_id']) ? $_SESSION['user_id'] : ''; ?>">
                 <input type="hidden" name="postId" value="<?php echo $postId; ?>">
-                <button id="likeButton" type="button" onclick="submitLikeForm()">Like</button>
+                <i  id="likeButton" onclick="submitLikeForm();toggleLike()" class="fa-regular fa-heart"></i>
             </form>
 
             <?php } else { ?>
