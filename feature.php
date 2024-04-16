10a928665916aa16aeceb7d4dc1c64d8c3fff2e9
NguyenCatNguyen <catngn11@gmail.com>
2024-04-16 01:50:22 -0500

    Update feature.php

--- feature.php
+++ feature.php
@@ -11,7 +11,15 @@ require_once("includes/postviewerutil.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Browser Blast</title>
     <link rel="stylesheet" href="feature.css">
-    <script src="./script.js" defer></script>
+    <script>
+      function redirectToLink(customLink) {
+          // Specify the URL you want to redirect to
+          var destination = customLink;
+          
+          // Redirect the user to the specified URL
+          window.location.href = destination;
+      }
+  </script>
 </head>
 
 <body>
@@ -19,26 +27,25 @@ require_once("includes/postviewerutil.php");
         <img src="./WebDisplay/Assests/Logo.png" class="logo_img" alt="">
         <ul class="nav_links">
             <li class="link link__hover-effect">
-              <a href="homepage.php">About</a>
+              <a href="feature.html">Experience</a>
             </li>
             <li class="link link__hover-effect">
               <a href="post.php">Upload Experience</a>
             </li>
-            <li class="link link__hover-effect">Favorites</li>
+            <li class="link link__hover-effect">Favorite</li>
             <?php if (!isset($_SESSION["user_id"])){ ?>
             <a href="index.php" class="link btn">
                 Sign In
             </a>
             <?php } else { ?>
               <a href="accountviewer.php?uid=<?php echo $_SESSION["user_id"]?>" class="link btn">
-                  <li><?php echo $_SESSION["user_username"];  ?></li>
+                <?php echo $_SESSION["user_username"];  ?>
               </a>
-              <li class="link link__hover-effect"></li>
             <?php } ?>
         </ul>
     </nav>
     <div class="feature">
-        <h1 class="title feature-title">Featured Experiences</h1>
+        <h1 class="title feature-title">Feature Experience</h1>
         <div class="feature-lists">
             <?php
             // Assuming you have a database connection named $pdo
@@ -56,7 +63,7 @@ require_once("includes/postviewerutil.php");
                 $imageSrc = './upload/' . $row['PostID'] . "." . $row['FileExt']; // Assuming image_filename column contains the filename
                 // Output the HTML structure for each post
                 ?>
-                <div class="feature-card">
+                <div class="feature-card" onclick="redirectToLink()">
                     <img class="card-img" src="<?php echo $imageSrc; ?>" alt="post image">
                     <div class="card-info">
                         <h4 class="title-card"><?php echo htmlspecialchars($row['Title']); ?></h4>
